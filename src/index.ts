699d9e6b6f595a0bb9d2c01d353b88536a3d0311
Prakash <prakash@nexthoughts.com>
2024-01-18 11:56:30 +0530

    initialized project

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// export * from "./redis/middlewares/page";
+// export * from "./redis/middlewares/project";
+// export * from "./redis/config";
+// export * from "./redis/paginate";
+// export * from "./redis/utils";
+export * from './library/methods'
---
+++ src/library/methods.ts
@@ -0,0 +1,7 @@
+import { NextFunction, Response } from 'express';
+// import { NextFunction, Response } from "express";
+export const helloWorld = () => {
+    return `Welocome to the Studio`
+}
+
+// export const get
