ec24dacbc39bb11bc6fd7b2b6a2953e4cc600c91
Prakash <prakash@nexthoughts.com>
2024-01-19 15:54:24 +0530

    added collection all methods

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,41 @@
-import { createItem, getAllItems } from "./methods/methods";
+import { bulkDeleteItems, createItem, deleteItemWithUuid, getAllItems, getItemWithUuid, getItemsByids, getItemsCountWithFilter, getItemsWithFilter, updateItemWithUuid } from "./methods/methods";
 
-export class DrapcodeApies {
-    private baseUrl: string;
+export class DrapcodeApis {
+    private project_seo_name: string;
     private xApiKey: string;
     private authorisation: string;
 
-    constructor(baseUrl: string, xApiKey: string = "", authorisation: string = "") {
-        this.baseUrl = baseUrl;
+    constructor(project_seo_name: string, xApiKey: string = "", authorisation: string = "") {
+        this.project_seo_name = project_seo_name;
         this.xApiKey = xApiKey;
         this.authorisation = authorisation;
     }
 
     async getAllItems(collectionName: string): Promise<any[]> {
-        return getAllItems(this.baseUrl, this.xApiKey, this.authorisation, collectionName);
+        return getAllItems(this.project_seo_name, this.xApiKey, this.authorisation, collectionName);
     }
     async createItem(collectionName: string, body: any): Promise<any[]> {
-        return createItem(this.baseUrl, this.xApiKey, this.authorisation, collectionName, body);
+        return createItem(this.project_seo_name, this.xApiKey, this.authorisation, collectionName, body);
+    }
+    async getItemsWithFilter(collectionName: string, filterUuid: string): Promise<any[]> {
+        return getItemsWithFilter(this.project_seo_name, this.xApiKey, this.authorisation, collectionName, filterUuid);
+    }
+    async getItemsCountWithFilter(collectionName: string, filterUuid: string): Promise<any[]> {
+        return getItemsCountWithFilter(this.project_seo_name, this.xApiKey, this.authorisation, collectionName, filterUuid);
+    }
+    async getItemWithUuid(collectionName: string, itemUuid: string): Promise<any[]> {
+        return getItemWithUuid(this.project_seo_name, this.xApiKey, this.authorisation, collectionName, itemUuid);
+    }
+    async updateItemWithUuid(collectionName: string, itemUuid: string, body: any): Promise<any[]> {
+        return updateItemWithUuid(this.project_seo_name, this.xApiKey, this.authorisation, collectionName, itemUuid, body);
+    }
+    async deleteItemWithUuid(collectionName: string, itemUuid: string): Promise<any[]> {
+        return deleteItemWithUuid(this.project_seo_name, this.xApiKey, this.authorisation, collectionName, itemUuid);
+    }
+    async bulkDeleteItems(collectionName: string, body: any): Promise<any[]> {
+        return bulkDeleteItems(this.project_seo_name, this.xApiKey, this.authorisation, collectionName, body);
+    }
+    async getItemsByids(collectionName: string, body: any): Promise<any[]> {
+        return getItemsByids(this.project_seo_name, this.xApiKey, this.authorisation, collectionName, body);
     }
 }
--- src/methods/methods.ts
+++ src/methods/methods.ts
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 export const getAllItems = async (
-    baseUrl: string,
+    project_seo_name: string,
     xApiKey: string,
     authorisation: string,
     collectionName: string
 ): Promise<any[]> => {
     try {
-        const url = `${baseUrl}/collection/${collectionName}/items`;
+        const url = `http://${project_seo_name}.api.prodelessdev.com:6002/api/v1/developer/collection/${collectionName}/items`;
         const response = await axios.get<any[]>(url, {
             headers: {
                 'x-api-key': xApiKey,
@@ -22,14 +22,14 @@ export const getAllItems = async (
 };
 
 export const createItem = async (
-    baseUrl: string,
+    project_seo_name: string,
     xApiKey: string,
     authorisation: string,
     collectionName: string,
     body: any
 ): Promise<any[]> => {
     try {
-        const url = `${baseUrl}/collection/${collectionName}/items`;
+        const url = `http://${project_seo_name}.api.prodelessdev.com:6002/api/v1/developer/collection/${collectionName}/items`;
         const response = await axios.post<any[]>(url, body, {
             headers: {
                 'x-api-key': xApiKey,
@@ -45,4 +45,157 @@ export const createItem = async (
 
 
 
+export const getItemsWithFilter = async (
+    project_seo_name: string,
+    xApiKey: string,
+    authorisation: string,
+    collectionName: string,
+    filterUuid: string
+): Promise<any[]> => {
+    try {
+        const url = `http://${project_seo_name}.api.prodelessdev.com:6002/api/v1/developer/collection/${collectionName}/filter/${filterUuid}/items`;
+        const response = await axios.get<any[]>(url, {
+            headers: {
+                'x-api-key': xApiKey,
+                'Authorization': authorisation,
+            },
+        });
+        return response.data;
+    } catch (error: any) {
+        console.error('Error fetching collection:', error.message);
+        throw error;
+    }
+};
 
+export const getItemsCountWithFilter = async (
+    project_seo_name: string,
+    xApiKey: string,
+    authorisation: string,
+    collectionName: string,
+    filterUuid: string
+): Promise<any[]> => {
+    try {
+        const url = `http://${project_seo_name}.api.prodelessdev.com:6002/api/v1/developer/collection/${collectionName}/filter/${filterUuid}/`;
+        const response = await axios.get<any[]>(url, {
+            headers: {
+                'x-api-key': xApiKey,
+                'Authorization': authorisation,
+            },
+        });
+        return response.data;
+    } catch (error: any) {
+        console.error('Error fetching collection:', error.message);
+        throw error;
+    }
+};
+
+export const getItemWithUuid = async (
+    project_seo_name: string,
+    xApiKey: string,
+    authorisation: string,
+    collectionName: string,
+    itemUuid: string
+): Promise<any[]> => {
+    try {
+        const url = `http://${project_seo_name}.api.prodelessdev.com:6002/api/v1/developer/collection/${collectionName}/item/${itemUuid}`;
+        const response = await axios.get<any[]>(url, {
+            headers: {
+                'x-api-key': xApiKey,
+                'Authorization': authorisation,
+            },
+        });
+        return response.data;
+    } catch (error: any) {
+        console.error('Error fetching collection:', error.message);
+        throw error;
+    }
+};
+
+export const updateItemWithUuid = async (
+    project_seo_name: string,
+    xApiKey: string,
+    authorisation: string,
+    collectionName: string,
+    itemUuid: string,
+    body: any
+): Promise<any[]> => {
+    try {
+        const url = `http://${project_seo_name}.api.prodelessdev.com:6002/api/v1/developer/collection/${collectionName}/item/${itemUuid}`;
+        const response = await axios.put<any[]>(url, body, {
+            headers: {
+                'x-api-key': xApiKey,
+                'Authorization': authorisation,
+            },
+        });
+        return response.data;
+    } catch (error: any) {
+        console.error('Error fetching collection:', error.message);
+        throw error;
+    }
+};
+
+export const deleteItemWithUuid = async (
+    project_seo_name: string,
+    xApiKey: string,
+    authorisation: string,
+    collectionName: string,
+    itemUuid: string
+): Promise<any[]> => {
+    try {
+        const url = `http://${project_seo_name}.api.prodelessdev.com:6002/api/v1/developer/collection/${collectionName}/item/${itemUuid}`;
+        const response = await axios.delete<any[]>(url, {
+            headers: {
+                'x-api-key': xApiKey,
+                'Authorization': authorisation,
+            },
+        });
+        return response.data;
+    } catch (error: any) {
+        console.error('Error fetching collection:', error.message);
+        throw error;
+    }
+};
+
+export const bulkDeleteItems = async (
+    project_seo_name: string,
+    xApiKey: string,
+    authorisation: string,
+    collectionName: string,
+    body: any
+): Promise<any[]> => {
+    try {
+        const url = `http://${project_seo_name}.api.prodelessdev.com:6002/api/v1/developer/collection/${collectionName}/bulkDelete`;
+        const response = await axios.post<any[]>(url, body, {
+            headers: {
+                'x-api-key': xApiKey,
+                'Authorization': authorisation,
+            },
+        });
+        return response.data;
+    } catch (error: any) {
+        console.error('Error fetching collection:', error.message);
+        throw error;
+    }
+};
+
+export const getItemsByids = async (
+    project_seo_name: string,
+    xApiKey: string,
+    authorisation: string,
+    collectionName: string,
+    body: any
+): Promise<any[]> => {
+    try {
+        const url = `http://${project_seo_name}.api.prodelessdev.com:6002/api/v1/developer/collection/${collectionName}/itemsbyids`;
+        const response = await axios.post<any[]>(url, body, {
+            headers: {
+                'x-api-key': xApiKey,
+                'Authorization': authorisation,
+            },
+        });
+        return response.data;
+    } catch (error: any) {
+        console.error('Error fetching collection:', error.message);
+        throw error;
+    }
+};
