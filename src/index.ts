02e0f514c1d4bd6d4493ea7e8e2c534d9808b810
Vijay Shukla <vshukla684@gmail.com>
2024-02-07 17:38:19 +0530

    migrate to fetch

--- src/index.ts
+++ src/index.ts
@@ -1,74 +1,137 @@
-import { bulkDeleteItems, createItem, deleteItemWithUuid, getAllItems, getItemWithUuid, getItemsByids, getItemsCountWithFilter, getItemsWithFilter, sendEmail, updateItemWithUuid } from "./methods/methods";
+import {
+  //   bulkDeleteItems,
+  createItem,
+  //   deleteItemWithUuid,
+  getAllItems,
+  getItemWithUuid,
+  //   getItemsByids,
+  getItemsCountWithFilter,
+  getItemsWithFilter,
+  //   sendEmail,
+  //   updateItemWithUuid,
+} from "./methods/methods";
 export class DrapcodeApis {
-    private project_seo_name: string;
-    private xApiKey: string;
-    private authorization: string; //authorization
-    private environment: string;
-    private API_PATH = 'drapcode.io/api/v1/developer';
+  private project_seo_name: string;
+  private xApiKey: string;
+  private authorization: string; //authorization
+  private environment: string;
+  private API_PATH = "drapcode.io/api/v1/developer";
 
-    constructor(project_seo_name: string, xApiKey: string = "", authorization: string = "", environment: string = 'PRODUCTION') {
-        this.project_seo_name = project_seo_name;
-        this.xApiKey = xApiKey;
-        this.authorization = authorization;
-        this.environment = environment
+  constructor(
+    project_seo_name: string,
+    xApiKey: string = "",
+    authorization: string = "",
+    environment: string = "PRODUCTION"
+  ) {
+    this.project_seo_name = project_seo_name;
+    this.xApiKey = xApiKey;
+    this.authorization = authorization;
+    this.environment = environment;
+  }
+  private getBaseUrl(): string {
+    switch (this.environment) {
+      case "PRODUCTION":
+        return `https://${this.project_seo_name}.api.${this.API_PATH}`;
+      case "PREVIEW":
+        return `https://${this.project_seo_name}.preview.${this.API_PATH}`;
+      case "BETA":
+        return `https://${this.project_seo_name}.beta.${this.API_PATH}`;
+      case "ALPHA":
+        return `https://${this.project_seo_name}.alpha.${this.API_PATH}`;
+      default:
+        return `https://${this.project_seo_name}.api.${this.API_PATH}`;
     }
-    private getBaseUrl(): string {
-        switch (this.environment) {
-            case 'PRODUCTION':
-                return `https://${this.project_seo_name}.api.${this.API_PATH}`;
-            case 'PREVIEW':
-                return `https://${this.project_seo_name}.preview.${this.API_PATH}`;
-            case 'BETA':
-                return `https://${this.project_seo_name}.beta.${this.API_PATH}`;
-            case 'ALPHA':
-                return `https://${this.project_seo_name}.alpha.${this.API_PATH}`;
-            default:
-                return `https://${this.project_seo_name}.api.${this.API_PATH}`;
-        }
+  }
+  private getHeaders(): Record<string, string> {
+    const headers: Record<string, string> = {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    };
+    if (this.xApiKey) {
+      headers["x-api-key"] = this.xApiKey;
     }
-    private getHeaders(): Record<string, string> {
-        const headers: Record<string, string> = {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-        };
-        if (this.xApiKey) {
-            headers['x-api-key'] = this.xApiKey;
-        }
-        if (this.authorization) {
-            headers['Authorization'] = this.authorization;
-        }
-        return headers;
+    if (this.authorization) {
+      headers["Authorization"] = this.authorization;
     }
+    return headers;
+  }
 
-
-    async getAllItems(collectionName: string): Promise<any> {
-        return getAllItems(this.getBaseUrl(), this.getHeaders(), collectionName);
-    }
-    async createItem(collectionName: string, body: any): Promise<any[]> {
-        return createItem(this.getBaseUrl(), this.getHeaders(), collectionName, body);
-    }
-    async getItemsWithFilter(collectionName: string, filterUuid: string): Promise<any[]> {
-        return getItemsWithFilter(this.getBaseUrl(), this.getHeaders(), collectionName, filterUuid,);
-    }
-    async getItemsCountWithFilter(collectionName: string, filterUuid: string): Promise<any[]> {
-        return getItemsCountWithFilter(this.getBaseUrl(), this.getHeaders(), collectionName, filterUuid);
-    }
-    async getItemWithUuid(collectionName: string, itemUuid: string): Promise<any[]> {
-        return getItemWithUuid(this.getBaseUrl(), this.getHeaders(), collectionName, itemUuid);
-    }
-    async updateItemWithUuid(collectionName: string, itemUuid: string, body: any): Promise<any[]> {
-        return updateItemWithUuid(this.getBaseUrl(), this.getHeaders(), collectionName, itemUuid, body);
-    }
-    async deleteItemWithUuid(collectionName: string, itemUuid: string): Promise<any[]> {
-        return deleteItemWithUuid(this.getBaseUrl(), this.getHeaders(), collectionName, itemUuid);
-    }
-    async bulkDeleteItems(collectionName: string, body: any): Promise<any[]> {
-        return bulkDeleteItems(this.getBaseUrl(), this.getHeaders(), collectionName, body);
-    }
-    async getItemsByids(collectionName: string, body: any): Promise<any[]> {
-        return getItemsByids(this.getBaseUrl(), this.getHeaders(), collectionName, body);
-    }
-    async sendEmail(templateId: string, sendTo: string): Promise<any[]> {
-        return sendEmail(this.getBaseUrl(), this.getHeaders(), templateId, sendTo)
-    }
+  async getAllItems(collectionName: string) {
+    return getAllItems(this.getBaseUrl(), this.getHeaders(), collectionName);
+  }
+  async createItem(collectionName: string, body: any) {
+    return createItem(
+      this.getBaseUrl(),
+      this.getHeaders(),
+      collectionName,
+      body
+    );
+  }
+  async getItemsWithFilter(collectionName: string, filterUuid: string) {
+    return getItemsWithFilter(
+      this.getBaseUrl(),
+      this.getHeaders(),
+      collectionName,
+      filterUuid
+    );
+  }
+  async getItemsCountWithFilter(collectionName: string, filterUuid: string) {
+    return getItemsCountWithFilter(
+      this.getBaseUrl(),
+      this.getHeaders(),
+      collectionName,
+      filterUuid
+    );
+  }
+  async getItemWithUuid(collectionName: string, itemUuid: string) {
+    return getItemWithUuid(
+      this.getBaseUrl(),
+      this.getHeaders(),
+      collectionName,
+      itemUuid
+    );
+  }
+  //   async updateItemWithUuid(
+  //     collectionName: string,
+  //     itemUuid: string,
+  //     body: any
+  //   ): Promise<any[]> {
+  //     return updateItemWithUuid(
+  //       this.getBaseUrl(),
+  //       this.getHeaders(),
+  //       collectionName,
+  //       itemUuid,
+  //       body
+  //     );
+  //   }
+  //   async deleteItemWithUuid(
+  //     collectionName: string,
+  //     itemUuid: string
+  //   ): Promise<any[]> {
+  //     return deleteItemWithUuid(
+  //       this.getBaseUrl(),
+  //       this.getHeaders(),
+  //       collectionName,
+  //       itemUuid
+  //     );
+  //   }
+  //   async bulkDeleteItems(collectionName: string, body: any): Promise<any[]> {
+  //     return bulkDeleteItems(
+  //       this.getBaseUrl(),
+  //       this.getHeaders(),
+  //       collectionName,
+  //       body
+  //     );
+  //   }
+  //   async getItemsByids(collectionName: string, body: any): Promise<any[]> {
+  //     return getItemsByids(
+  //       this.getBaseUrl(),
+  //       this.getHeaders(),
+  //       collectionName,
+  //       body
+  //     );
+  //   }
+  //   async sendEmail(templateId: string, sendTo: string): Promise<any[]> {
+  //     return sendEmail(this.getBaseUrl(), this.getHeaders(), templateId, sendTo);
+  //   }
 }
--- src/methods/methods.ts
+++ src/methods/methods.ts
@@ -1,227 +1,263 @@
-import axios from 'axios';
-
 const createErrorResponse = (error: any) => {
-    if (error.response && error.response.status === 404) {
-        const responseData = error.response.data
-        let finalData;
-        if (responseData == 'This url does not exist. Please publish again.') {
-            finalData = "Please check your project name or publish again."
-        } else if (responseData.message) {
-            finalData = responseData.message
-        } else {
-            finalData = responseData !== "" ? responseData : "Not found"
-        }
-        return {
-            code: error.response.status,
-            success: false,
-            data: finalData,
-            error: '',
-            message: '',
-        };
-    } else if (error.response && error.response.status === 401) {
-        const responseData = error.response
-        return {
-            code: responseData.status,
-            success: false,
-            data: responseData.data.message,
-            error: '',
-            message: '',
-        }
-    } else if (error.response && error.response.status === 400) {
-        const responseData = error.response
-        return {
-            code: responseData.status,
-            success: false,
-            data: "Please Check Your Credentials",
-            error: '',
-            message: '',
-        }
+  if (error.response && error.response.status === 404) {
+    const responseData = error.response.data;
+    let finalData;
+    if (responseData == "This url does not exist. Please publish again.") {
+      finalData = "Please check your project name or publish again.";
+    } else if (responseData.message) {
+      finalData = responseData.message;
+    } else {
+      finalData = responseData !== "" ? responseData : "Not found";
     }
     return {
-        code: error.response.code,
-        success: false,
-        data: "",
-        error: 'Please check your project name or publish again.',
-        message: '',
+      code: error.response.status,
+      success: false,
+      data: finalData,
+      error: "",
+      message: "",
     };
+  } else if (error.response && error.response.status === 401) {
+    const responseData = error.response;
+    return {
+      code: responseData.status,
+      success: false,
+      data: responseData.data.message,
+      error: "",
+      message: "",
+    };
+  } else if (error.response && error.response.status === 400) {
+    const responseData = error.response;
+    return {
+      code: responseData.status,
+      success: false,
+      data: "Please Check Your Credentials",
+      error: "",
+      message: "",
+    };
+  }
+  return {
+    code: error.response.code,
+    success: false,
+    data: "",
+    error: "Please check your project name or publish again.",
+    message: "",
+  };
 };
 
+const processResponse = (result: any) => {
+  console.log("result", result);
+  if (result.status === "FAILED") {
+    if (result.error) {
+      if (result.error.errStatus === 401) {
+        return {
+          code: result.error.errStatus,
+          success: false,
+          error: result.error.message,
+          message: result.error.message,
+          data: "",
+        };
+      }
+    }
+    return {
+      code: 400,
+      success: false,
+      error: "API Failed",
+      message: "",
+      data: result,
+    };
+  } else {
+    if (result.code === 404) {
+      return {
+        code: result.code,
+        success: false,
+        error: result.data,
+        message: result.data,
+        data: "",
+      };
+    }
+    return { code: 200, success: true, error: "", message: "", data: result };
+  }
+};
 
 export const getAllItems = async (
-    baseurl: string,
-    headers: Record<string, string>,
-    collectionName: string,
-): Promise<any> => {
-    try {
-        const url = `${baseurl}/collection/${collectionName}/items`;
-        const response = await axios.get<any>(url, { headers });
-        if (response.data && response.data.code === 404) {
-            return { success: false, data: response.data.data, error: '', message: '' };
-        }
-        return { success: true, data: response.data, error: '', message: '' };
-    } catch (error: any) {
-        return createErrorResponse(error)
-    }
+  baseurl: string,
+  headers: Record<string, string>,
+  collectionName: string
+) => {
+  try {
+    const url = `${baseurl}/collection/${collectionName}/items`;
+    console.log("url", url);
+    const response = await fetch(url, { method: "GET", headers });
+    const result = await response.json();
+    return processResponse(result);
+  } catch (error: any) {
+    console.log("error", error);
+    return createErrorResponse(error);
+  }
 };
 
 export const createItem = async (
-    baseurl: string,
-    headers: Record<string, string>,
-    collectionName: string,
-    body: any,
-): Promise<any> => {
-    try {
-        const url = `${baseurl}/collection/${collectionName}/items`;
-        const response = await axios.post<any>(url, body, {
-            headers
-        });
-        if (response.data && response.data.code === 404) {
-            return { success: false, data: response.data.data, error: '', message: '' };
-        }
-        return { success: true, data: response.data, error: '', message: '' };
-    } catch (error: any) {
-        return createErrorResponse(error)
+  baseurl: string,
+  headers: Record<string, string>,
+  collectionName: string,
+  body: any
+) => {
+  try {
+    const url = `${baseurl}/collection/${collectionName}/items`;
+    const response = await fetch(url, { method: "POST", headers, body: body });
+    const result = await response.json();
+    if (result.data && result.data.code === 404) {
+      return {
+        success: false,
+        data: result.data.data,
+        error: "",
+        message: "",
+      };
     }
+    return { success: true, data: result.data, error: "", message: "" };
+  } catch (error: any) {
+    return createErrorResponse(error);
+  }
 };
 
-
-
 export const getItemsWithFilter = async (
-    baseurl: string,
-    headers: Record<string, string>,
-    collectionName: string,
-    filterUuid: string,
-): Promise<any> => {
-    try {
-        const url = `${baseurl}/collection/${collectionName}/filter/${filterUuid}/items`;
-        const response = await axios.get<any>(url, {
-            headers
-        });
-        return { success: true, data: response.data, error: '', message: '' };
-    } catch (error: any) {
-        return createErrorResponse(error)
-    }
+  baseurl: string,
+  headers: Record<string, string>,
+  collectionName: string,
+  filterUuid: string
+) => {
+  try {
+    const url = `${baseurl}/collection/${collectionName}/filter/${filterUuid}/items`;
+    const response = await fetch(url, { method: "GET", headers });
+    const result = await response.json();
+    return { success: true, data: result.data, error: "", message: "" };
+  } catch (error: any) {
+    return createErrorResponse(error);
+  }
 };
 
 export const getItemsCountWithFilter = async (
-    baseurl: string,
-    headers: Record<string, string>,
-    collectionName: string,
-    filterUuid: string,
-): Promise<any> => {
-    try {
-        const url = `${baseurl}/collection/${collectionName}/filter/${filterUuid}/count`;
-        const response = await axios.get<any[]>(url, {
-            headers
-        });
-        return { success: true, data: response.data, error: '', message: '' };
-    } catch (error: any) {
-        return createErrorResponse(error)
-    }
+  baseurl: string,
+  headers: Record<string, string>,
+  collectionName: string,
+  filterUuid: string
+) => {
+  try {
+    const url = `${baseurl}/collection/${collectionName}/filter/${filterUuid}/count`;
+    const response = await fetch(url, { method: "GET", headers });
+    const result = await response.json();
+    return { success: true, data: result.data, error: "", message: "" };
+  } catch (error: any) {
+    return createErrorResponse(error);
+  }
 };
 
 export const getItemWithUuid = async (
-    baseurl: string,
-    headers: Record<string, string>,
-    collectionName: string,
-    itemUuid: string,
-): Promise<any> => {
-    try {
-        const url = `${baseurl}/collection/${collectionName}/item/${itemUuid}`;
-        const response = await axios.get<any[]>(url, {
-            headers
-        });
-        return { success: true, data: response.data, error: '', message: '' };
-    } catch (error: any) {
-        return createErrorResponse(error)
-    }
+  baseurl: string,
+  headers: Record<string, string>,
+  collectionName: string,
+  itemUuid: string
+) => {
+  try {
+    const url = `${baseurl}/collection/${collectionName}/item/${itemUuid}`;
+    const response = await fetch(url, { method: "GET", headers });
+    const result = await response.json();
+    return { success: true, data: result.data, error: "", message: "" };
+  } catch (error: any) {
+    return createErrorResponse(error);
+  }
 };
-
+/*
 export const updateItemWithUuid = async (
-    baseurl: string,
-    headers: Record<string, string>,
-    collectionName: string,
-    itemUuid: string,
-    body: any,
+  baseurl: string,
+  headers: Record<string, string>,
+  collectionName: string,
+  itemUuid: string,
+  body: any
 ): Promise<any> => {
-    try {
-        const url = `${baseurl}/collection/${collectionName}/item/${itemUuid}`;
-        const response = await axios.put<any>(url, body, {
-            headers
-        });
-        return { success: true, data: response.data, error: '', message: '' };
-    } catch (error: any) {
-        return createErrorResponse(error)
-    }
+  try {
+    const url = `${baseurl}/collection/${collectionName}/item/${itemUuid}`;
+    const response = await axios.put<any>(url, body, {
+      headers,
+    });
+    return { success: true, data: response.data, error: "", message: "" };
+  } catch (error: any) {
+    return createErrorResponse(error);
+  }
 };
 
 export const deleteItemWithUuid = async (
-    baseurl: string,
-    headers: Record<string, string>,
-    collectionName: string,
-    itemUuid: string,
+  baseurl: string,
+  headers: Record<string, string>,
+  collectionName: string,
+  itemUuid: string
 ): Promise<any> => {
-    try {
-        const url = `${baseurl}/collection/${collectionName}/item/${itemUuid}`;
-        const response = await axios.delete<any>(url, {
-            headers
-        });
-        return { success: true, data: response.data?.message, error: '', message: '' };
-    } catch (error: any) {
-        return createErrorResponse(error)
-    }
+  try {
+    const url = `${baseurl}/collection/${collectionName}/item/${itemUuid}`;
+    const response = await axios.delete<any>(url, {
+      headers,
+    });
+    return {
+      success: true,
+      data: response.data?.message,
+      error: "",
+      message: "",
+    };
+  } catch (error: any) {
+    return createErrorResponse(error);
+  }
 };
 
 export const bulkDeleteItems = async (
-    baseurl: string,
-    headers: Record<string, string>,
-    collectionName: string,
-    body: any,
+  baseurl: string,
+  headers: Record<string, string>,
+  collectionName: string,
+  body: any
 ): Promise<any> => {
-    try {
-        const url = `${baseurl}/collection/${collectionName}/bulkDelete`;
-        const response = await axios.post<any>(url, body, {
-            headers
-        });
-        return { success: true, data: response.data, error: '', message: '' };
-    } catch (error: any) {
-        return createErrorResponse(error)
-    }
+  try {
+    const url = `${baseurl}/collection/${collectionName}/bulkDelete`;
+    const response = await axios.post<any>(url, body, {
+      headers,
+    });
+    return { success: true, data: response.data, error: "", message: "" };
+  } catch (error: any) {
+    return createErrorResponse(error);
+  }
 };
 
 export const getItemsByids = async (
-    baseurl: string,
-    headers: Record<string, string>,
-    collectionName: string,
-    body: any,
+  baseurl: string,
+  headers: Record<string, string>,
+  collectionName: string,
+  body: any
 ): Promise<any> => {
-    try {
-        const url = `${baseurl}/collection/${collectionName}/itemsbyids`;
-        const response = await axios.post<any>(url, body, {
-            headers
-        });
-        return { success: true, data: response.data, error: '', message: '' };
-    } catch (error: any) {
-        return createErrorResponse(error)
-    }
+  try {
+    const url = `${baseurl}/collection/${collectionName}/itemsbyids`;
+    const response = await axios.post<any>(url, body, {
+      headers,
+    });
+    return { success: true, data: response.data, error: "", message: "" };
+  } catch (error: any) {
+    return createErrorResponse(error);
+  }
 };
 export const sendEmail = async (
-    baseurl: string,
-    headers: Record<string, string>,
-    templateId: string,
-    sendTo: any,
+  baseurl: string,
+  headers: Record<string, string>,
+  templateId: string,
+  sendTo: any
 ): Promise<any> => {
-    try {
-        const url = `${baseurl}/sendEmail/${templateId}/user/${sendTo}`;
-        const response = await axios.post<any[]>(url, "", {
-            headers
-        });
-        return { success: true, data: response.data, error: '', message: '' }
-    } catch (error: any) {
-        return createErrorResponse(error)
-    }
+  try {
+    const url = `${baseurl}/sendEmail/${templateId}/user/${sendTo}`;
+    const response = await axios.post<any[]>(url, "", {
+      headers,
+    });
+    return { success: true, data: response.data, error: "", message: "" };
+  } catch (error: any) {
+    return createErrorResponse(error);
+  }
 };
-
+*/
 /**
  * {
  * code,
