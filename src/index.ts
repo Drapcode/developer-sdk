01d7180c761ffa8b7dc7638e2aefedca4dfca89e
Vijay Shukla <vshukla684@gmail.com>
2025-10-03 12:59:38 +0530

    add method to load only item not reference

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import {
   deleteFieldItem,
   deleteItemWithUuid,
   getAllItems,
+  getAllItemsOnly,
   getItemOnly,
   getItemWithUuid,
   getItemsByids,
@@ -219,6 +220,13 @@ export class DrapcodeApis {
   ) {
     return this.callApi(getAllItems, collectionName, reqQuery, query);
   }
+  async getAllItemsOnly(
+    collectionName: string,
+    reqQuery: SearchPaginate | any = null,
+    query: Query[] | [] = []
+  ) {
+    return this.callApi(getAllItemsOnly, collectionName, reqQuery, query);
+  }
   async createItem(collectionName: string, body: any) {
     return this.callApi(createItem, collectionName, body);
   }
@@ -234,6 +242,7 @@ export class DrapcodeApis {
   async getItemOnly(collectionName: string, itemUuid: string) {
     return this.callApi(getItemOnly, collectionName, itemUuid);
   }
+
   async countItemByValue(
     collectionName: string,
     fieldName: string,
